a04e92d2cf361c6b34c592fa9ea911a7c0444e57
RohilS23 <rohil.shah.one@gmail.com>
2024-10-25 15:54:41 -0700

    tets

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 using namespace std;
 
+int testMethod() {
+    int i = 3;
+    int j = 3;
+    if (i == j)
+        return i;
+    else
+        return 0;
+}
+
 int main () {
     cout << "hello world" << endl;
     cout << "!" << endl;
     cout << "hi " <<endl;
     cout << "is it working" << endl;
+    cout << testMethod() << endl;
 }
+
