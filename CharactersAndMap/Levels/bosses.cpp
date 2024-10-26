21c90d51edfec1abaa3e25941d4955a87a6afa03
UsingWASD <thiskevinprofessional@gmail.com>
2024-10-25 18:28:24 -0700

    MORE CONTENT

---
+++ CharactersAndMap/Levels/bosses.cpp
@@ -0,0 +1,4 @@
+//10 bosses total, designs get a bit more complex 
+//first boss can be like base color octogon, 2nd could be a slightly bigger 
+//octogon with circles embeded inside, ,later octogons can have mini-player model triangles trapped
+//first apperance ever 5 waves, each boss drops 1 guarnteed lvl  for player
---
+++ Functionality/Leveling/Skills/ninjaTabi.cpp
@@ -0,0 +1,2 @@
+//lvl 1, dodges 1 attack from any enemy, invulnerable from that single attack, recharges every 10 seconds
+//lvl5, 10/10 dodge charges, recharges every 6 seconds
