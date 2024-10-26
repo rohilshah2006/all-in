baec5a98e54194588c9e769266002b7d807cb2ce
UsingWASD <thiskevinprofessional@gmail.com>
2024-10-25 17:50:02 -0700

    k

---
+++ Functionality/Leveling/leveling/levelingFormula.cpp
@@ -0,0 +1,5 @@
+//amoung for leveling exp req, e.g. lvl1 to lvl2 will be 500 exp req
+//lvl 2 to 3 could be like 800
+//i'm thinking, +300 exp req for each lvl 1-10
+//+500 for 10-20 maybe
+//1.05x the original amount each upgrade there on?
